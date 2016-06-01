0b52bdd06fcd532626aa9fc1f0c3fade25693e50
LightbulbProductions <gerbermichelle23@gmail.com>
2016-06-01 17:17:47 -0400

    delete not working error with edit

--- app/controllers/text_posts_controller.rb
+++ app/controllers/text_posts_controller.rb
@@ -1,28 +1,30 @@
 class TextPostsController < ApplicationController
-    def index
-      @text_posts = TextPost.all
-    end
+    before_action :set_text_post, only: [:show, :edit, :update, :delete]
 
-    def show
-      @text_post = TextPost.find(id)
-    end
+      def index
+        @text_posts = TextPost.all
+      end
 
-    def new
-      @text_post = TextPost.new
-    end
+      def new
+        @text_post = TextPost.new
+      end
 
-    def edit
-      @text_post = TextPost.find(id)
-    end
-
-    def create
-      @text_post = TextPost.new(text_post_params)
-        if @text_post.save
-          redirect_to text_posts_path
-        else
-          @errors = @text_post.errors
+      def create
+        @text_post = TextPost.new(text_post_params)
+          if @text_post.save
+            redirect_to text_posts_path
+          else
+            @errors = @text_post.errors
             render :new
         end
+      end
+
+      def show
+        @text_post = TextPost.find(id)
+      end
+
+      def edit
+          @text_post = TextPost.find(id)
       end
 
     def update
@@ -37,12 +39,17 @@ class TextPostsController < ApplicationController
 
     def delete
       @text_post = TextPost.find(id)
-       if @text_post.destroy
-        redirect_to text_posts_path
-      end
+      if @text_post.delete(text_post_params)
+        render :destroy
+      else
+       redirect_to text_posts_path
+     end
     end
 
     private
+      def set_text_post
+        @text_posts = TextPost.find(id)
+      end
       def text_post_params
         params.require(:text_post).permit(:url, :title, :blog)
       end
